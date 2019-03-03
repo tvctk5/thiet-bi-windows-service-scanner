743d2ec5adf24227c13657a63d2327171a12d4be
tvctk5 <trai.it.utehy@gmail.com>
2019-03-03 10:32:48 +0700

    Update ReleaseVesion: 20190228

--- Scanner_Service/install.bat
+++ Scanner_Service/install.bat
@@ -4,8 +4,8 @@ REM The following directory is for .NET 4.0
 set DOTNETFX2=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319
 set PATH=%PATH%;%DOTNETFX2%
 
-echo Installing Scanner_Service.exe...
+echo Installing C:\Services\Scanner\Scanner_Service.exe...
 echo ---------------------------------------------------
-InstallUtil -i Scanner_Service.exe
+InstallUtil -i C:\Services\Scanner\Scanner_Service.exe
 echo ---------------------------------------------------
 echo Done.
--- Scanner_Service/uninstall.bat
+++ Scanner_Service/uninstall.bat
@@ -4,8 +4,8 @@ REM The following directory is for .NET 4.0
 set DOTNETFX2=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319
 set PATH=%PATH%;%DOTNETFX2%
 
-echo Uninstalling Scanner_Service.exe...
+echo Uninstalling C:\Services\Scanner\Scanner_Service.exe...
 echo ---------------------------------------------------
-InstallUtil -u Scanner_Service.exe
+InstallUtil -u C:\Services\Scanner\Scanner_Service.exe
 echo ---------------------------------------------------
 echo Done.
